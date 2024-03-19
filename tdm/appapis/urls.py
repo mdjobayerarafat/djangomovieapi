5d5ca0de09e793a39a7471ff38df05c57f301137
Jubayer Arafat <mdjobayerarafat@gmail.com>
2024-03-20 00:37:05 +0600

    hi5

--- tdm/appapis/urls.py
+++ tdm/appapis/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path , include
-from .views import movie_list
+from .views import movie_list, movie_details
 urlpatterns = [
    path('list/',movie_list,name='movie-list'),
+   path('<int:pk>', movie_details,name='movie-detail'),
 ]
